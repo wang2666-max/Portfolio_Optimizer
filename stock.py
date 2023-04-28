c5d3bffa02372094b58f16b25c74f0ba8fa77cd1
Jason <jw6542@nyu.edu>
2023-04-28 16:52:26 -0400

    version

--- stock.py
+++ stock.py
@@ -13,13 +13,16 @@ class Stock:
         
         mkt_date = datetime.strptime(mkt_date, '%Y-%m-%d')
         self.mkt_date= Date(mkt_date.day, mkt_date.month, mkt_date.year)
+
+        self.years=time_frame
         
         self.time_frame = Period(-1*time_frame,Years)
 
         self.term_dates = None
         self.term_price = None
-        self.annualized_return = None
-        self.annualized_vol = None
+        self.df=None
+        
+    
         self._fetch_data()
 
     def _fetch_data(self):
@@ -28,16 +31,25 @@ class Stock:
         data = yahoo_financials.get_historical_price_data(start_date=start.to_date().strftime('%Y-%m-%d'), end_date=self.mkt_date.to_date().strftime('%Y-%m-%d'), time_interval='daily')
         prices = [day['adjclose'] for day in data[self.ticker]['prices']]
         dates = [x['formatted_date'] for x in data[self.ticker]['prices']]
+        print(dates[0])
+        df1=pd.DataFrame()
+        df1["dates"]=dates
+        df1["price"]=prices
+        df1["daily_return"]=np.log(df1["price"]/df1["price"].shift())
+
+        print(df1.head())
+        self.df=df1
         self.term_price = np.array(prices)
         self.term_dates = pd.to_datetime(dates)
 
     def calculate_annualized_return(self):
-        daily_returns = (self.term_price[1:] / self.term_price[:-1]) - 1
-        total_return = np.prod(daily_returns + 1) - 1
-        num_years = len(self.term_dates) / 365
-        self.annualized_return = (1 + total_return) ** (1 / num_years) - 1
+        
+        total_return = np.log(list(self.df["price"])[-1] / list(self.df["price"])[0]) 
+        
+        self.annualized_return = total_return / self.years
 
     def calculate_annualized_vol(self):
-        daily_returns = (self.term_price[1:] / self.term_price[:-1]) - 1
-        self.annualized_vol = np.std(daily_returns) * np.sqrt(252)
+        total_vol=self.df["daily_return"].std()
+        
+        self.annualized_vol = total_vol * np.sqrt(252)
 
